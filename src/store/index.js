6fed4a7401922c56303aa0ff2003fe80d0dd54c6
Harumitsu Fujii <dany.ubi@gmail.com>
2019-08-12 17:43:34 +0900

    add videos store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import articles from "./articles";
+import videos from "./videos";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     articles,
+    videos
   }
 });
---
+++ src/store/videos/mutations.js
@@ -0,0 +1,5 @@
+export default {
+  setVideos(state, videos) {
+    state.videos = videos;
+  }
+};
