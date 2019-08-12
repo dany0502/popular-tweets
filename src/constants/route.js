e146a3183f478a109432f328d6644a2ffb9ff01a
Harumitsu Fujii <harumitsu.fujii@linecorp.com>
2019-08-12 17:17:42 +0900

    add router settings

---
+++ src/constants/route.js
@@ -0,0 +1,8 @@
+export const ARTICLES = {
+  name: "articles",
+  path: "/articles"
+};
+export const VIDEOS = {
+  name: "videos",
+  path: "/videos"
+};
---
+++ src/views/Videos.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Videos</h1>
+  </div>
+</template>
