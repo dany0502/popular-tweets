b72ecddd06e80c5438ac6539c37028c5ac0742a2
Harumitsu Fujii <dany.ubi@gmail.com>
2019-08-13 21:08:55 +0900

    Implement header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    <div id="wrapper">
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <header-bar />
+          <router-view class="adjustContentRight"></router-view>
+        </div>
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright © bazz tweet 2019</span>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <a class="scroll-to-top rounded" href="#page-top" style="display: none;">
+      <i class="fas fa-angle-up"></i>
+    </a>
   </div>
 </template>
+<script>
+import HeaderBar from "@/components/Header";
 
+export default {
+  name: "App",
+  components: {
+    HeaderBar
+  }
+};
+</script>
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav
+    class="headerContainer navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow"
+  >
+    <button
+      @click="$emit('click')"
+      class="btn btn-link d-md-none rounded-circle mr-3"
+    >
+      <i class="fa fa-bars"></i>
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
