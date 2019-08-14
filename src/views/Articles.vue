bbd6d5b571fa123a5c90a1c3620ffa7c79ce371a
Harumitsu Fujii <dany.ubi@gmail.com>
2019-08-14 12:40:20 +0900

    Show user profile image in Article view

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -5,7 +5,10 @@
     </div>
     <div v-for="(tweet, index) in tweets" :key="index" class="card shadow mb-4">
       <div class="card-header py-3">
-        <img class="img img-profile rounded-circle" src="" />
+        <img
+          class="img img-profile rounded-circle"
+          :src="tweet.user.profile_image_url_https"
+        />
         <h6 class="m-0 ml-3 font-weight-bold text-primary inline">
           {{ tweet.user.name }}
         </h6>
