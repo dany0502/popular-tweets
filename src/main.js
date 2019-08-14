0d3c160b0f23d061ee8c497b2ada60f11cc96c97
Harumitsu Fujii <dany.ubi@gmail.com>
2019-08-14 19:27:37 +0900

    Implement Vue error handler

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "@/store";
 import lazyLoad from "@/directives/lazy-load";
+import vueErrorHandler from "@/errorHandlers/vue";
 import "@/utils/request-manager";
 
+Vue.config.errorHandler = vueErrorHandler;
 Vue.config.productionTip = false;
 Vue.directive("lazy-load", lazyLoad);
 
