4eae042fa53bd86f5a03a69d907c23d02415db9f
Harumitsu Fujii <dany.ubi@gmail.com>
2019-08-15 00:34:33 +0900

    Implement Videos view

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>Videos</h1>
+  <div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+      <h1 class="h3 mb-0 text-gray-800">Videos</h1>
+    </div>
+    <div v-for="(tweet, index) in tweets" :key="index" class="card shadow mb-4">
+      <div class="card-header py-3">
+        <img
+          width="48px"
+          height="48px"
+          v-lazy-load
+          class="img img-profile rounded-circle"
+          :data-src="tweet.user.profile_image_url_https"
+        />
+        <span class="m-0 ml-3 font-weight-bold text-primary inline">
+          {{ tweet.user.name }}
+        </span>
+      </div>
+      <div class="card-body">
+        <p>{{ tweet.text }}</p>
+        <p>
+          <i class="mr-1 fas fa-retweet"></i
+          ><span>{{ tweet.retweet_count }}</span
+          ><i class="ml-4 mr-1 fas fa-heart"></i
+          ><span>{{ tweet.favorite_count }}</span>
+        </p>
+        <a v-if="getUrl(tweet) !== null" :href="getUrl(tweet)">もっと見る</a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Videos",
+  computed: {
+    ...mapState("videos", ["tweets"])
+  },
+  methods: {
+    getUrl(tweet) {
+      return (tweet.entities.urls[0] && tweet.entities.urls[0].url) || null;
+    }
+  }
+};
+</script>
