89e4625c2bc53ce370ed9cfea8ec3576a302be28
Harumitsu Fujii <dany.ubi@gmail.com>
2019-08-20 00:18:12 +0900

    Serve static files from dist directory

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const articles = require("./controllers/articles");
 const videos = require("./controllers/videos");
 const app = express();
 
+app.use(express.static("dist"));
 app.use("/api/getarticles", articles);
 app.use("/api/getvideos", videos);
 
